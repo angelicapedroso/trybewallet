0fbf57b51655d3e733215aefd5fd34394483aaf1
Angelica <angelicapedroso05@gmail.com>
2022-01-10 19:09:52 -0300

    feat(Wallet):add header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <header>
+        <ul>
+          <li>E-mail</li>
+          <li
+            data-testid="total-field"
+          >
+            Despesa Total: 0
+          </li>
+          <li>BRL</li>
+        </ul>
+      </header>
+    );
   }
 }
 
