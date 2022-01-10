c53325170b39dbe05f7ac3e2feba8511d149266a
Angelica <angelicapedroso05@gmail.com>
2022-01-10 18:29:47 -0300

    feat(Login):add validation on inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+      redirect: false,
+    };
+
+    this.validationInputs = this.validateInputs.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.validationInputs();
+    });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.setState({
+      redirect: true,
+    });
+  }
+
+  validateInputs() {
+    const { email, password } = this.state;
+    const SIX = 6;
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (email.match(regex) && password.length >= SIX) {
+      return this.setState({ disabled: false });
+    } return this.setState({ disabled: true });
+  }
+  // validation reference with regex: https://www.ti-enxame.com/pt/javascript/validacao-de-e-mail-de-expressao-regular-em-javascript/957575053/
+
   render() {
+    const { email, password, disabled, redirect } = this.state;
     return (
       <form>
         <label htmlFor="email">
@@ -10,6 +53,8 @@ class Login extends React.Component {
             name="email"
             data-testid="email-input"
             placeholder="e-mail"
+            value={ email }
+            onChange={ this.handleChange }
           />
         </label>
         <label htmlFor="password">
@@ -18,13 +63,18 @@ class Login extends React.Component {
             name="password"
             data-testid="password-input"
             placeholder="senha"
+            value={ password }
+            onChange={ this.handleChange }
           />
         </label>
         <button
           type="submit"
+          disabled={ disabled }
+          onClick={ this.handleClick }
         >
           Entrar
         </button>
+        { redirect && <Redirect to="/carteira" /> }
       </form>
     );
   }
