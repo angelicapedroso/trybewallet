91094e9f462914d5c5232e7b24d4540b545bc7b0
Angelica <angelicapedroso05@gmail.com>
2022-01-10 16:42:34 -0300

    feat(Login):add login fields

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="email">
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            placeholder="e-mail"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            placeholder="senha"
+          />
+        </label>
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
