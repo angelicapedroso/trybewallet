a97d13bd7498d5c03397d67e0fe56fc4d845d283
Angelica <angelicapedroso05@gmail.com>
2022-01-11 01:21:07 -0300

    feat: implement action and user reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
-const SAVE_EMAIL = 'SAVE_EMAIL';
+export const SAVE_EMAIL = 'SAVE_EMAIL';
 
-const saveEmail = (email) => ({
+export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
   payload: email,
 });
-
-export default saveEmail;
